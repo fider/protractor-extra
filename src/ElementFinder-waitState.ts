8929b5ee0096b18d09c4d5d3243653a95c6ef526
Wojciech Fiderek <wfiderek@lenovo.com>
2019-08-05 15:06:10 +0200

    improved error handling (missing async keyword)

--- src/ElementFinder-waitState.ts
+++ src/ElementFinder-waitState.ts
@@ -7,17 +7,17 @@ import { waitTextIs, waitTextContains, waitTextInValueIs, waitSelected, waitInvi
 //      Implementation:  content/attribute check
 // ---------------------------------------------------------
 
-ElementFinder.prototype.waitTextContains = function _waitTextContains(text: string | RegExp, timeoutMs?: string | number) {
+ElementFinder.prototype.waitTextContains = async function _waitTextContains(text: string | RegExp, timeoutMs?: string | number) {
     timeoutMs = timeoutMs || this.options.timeouts.waitTextContains;
     waitTextContains(this, text, timeoutMs);
 };
 
-ElementFinder.prototype.waitTextIs = function _waitTextIs(text: string | RegExp, timeoutMs?: string | number) {
+ElementFinder.prototype.waitTextIs = async function _waitTextIs(text: string | RegExp, timeoutMs?: string | number) {
     timeoutMs = timeoutMs || this.options.timeouts.waitTextIs;
     waitTextIs(this, text, timeoutMs);
 };
 
-ElementFinder.prototype.waitTextInValueIs = function _waitTextInValueIs(text: string | RegExp, timeoutMs?: string | number) {
+ElementFinder.prototype.waitTextInValueIs = async function _waitTextInValueIs(text: string | RegExp, timeoutMs?: string | number) {
     timeoutMs = timeoutMs || this.options.timeouts.waitTextInValueIs;
     waitTextInValueIs(this, text, timeoutMs);
 };
@@ -28,32 +28,32 @@ ElementFinder.prototype.waitTextInValueIs = function _waitTextInValueIs(text: st
 //      Implementation:  state check
 // ---------------------------------------------------------
 
-ElementFinder.prototype.waitClickable = function _waitClickable(this: ElementFinder, timeoutMs?: string | number) {
+ElementFinder.prototype.waitClickable = async function _waitClickable(this: ElementFinder, timeoutMs?: string | number) {
     timeoutMs = timeoutMs || this.options.timeouts.waitClickable;
     waitClickable(this, timeoutMs);
 };
 
-ElementFinder.prototype.waitPresent = function _waitPresent(timeoutMs?: string | number) {
+ElementFinder.prototype.waitPresent = async function _waitPresent(timeoutMs?: string | number) {
     timeoutMs = timeoutMs || this.options.timeouts.waitPresent;
     waitPresent(this, timeoutMs);
 };
 
-ElementFinder.prototype.waitStale = function _waitStale(timeoutMs?: string | number) {
+ElementFinder.prototype.waitStale = async function _waitStale(timeoutMs?: string | number) {
     timeoutMs = timeoutMs || this.options.timeouts.waitStale;
     waitStale(this, timeoutMs);
 };
 
-ElementFinder.prototype.waitVisible = function _waitVisible(timeoutMs?: string | number) {
+ElementFinder.prototype.waitVisible = async function _waitVisible(timeoutMs?: string | number) {
     timeoutMs = timeoutMs || this.options.timeouts.waitVisible;
     waitVisible(this, timeoutMs);
 };
 
-ElementFinder.prototype.waitInvisible = function _waitInvisible(timeoutMs?: string | number) {
+ElementFinder.prototype.waitInvisible = async function _waitInvisible(timeoutMs?: string | number) {
     timeoutMs = timeoutMs || this.options.timeouts.waitInvisible;
     waitInvisible(this, timeoutMs);
 };
 
-ElementFinder.prototype.waitSelected = function _waitSelected(timeoutMs?: string | number) {
+ElementFinder.prototype.waitSelected = async function _waitSelected(timeoutMs?: string | number) {
     timeoutMs = timeoutMs || this.options.timeouts.waitSelected;
     waitSelected(this, timeoutMs);
 };
